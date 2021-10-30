e8b175f516bc7b3b182788aad6819a5d5c3ca521
Weaknessxyz <eliasknks@gmail.com>
2021-10-30 21:08:06 +0200

    Add 'New Grid' button to select up to 100 squares per side in the grid

--- script.js
+++ script.js
@@ -2,16 +2,39 @@ const container = document.createElement('div');
 container.classList.add('grid-container');
 document.body.appendChild(container);
 
-for (i = 1; i <= 256; i++) {
-    container.appendChild(document.createElement('div'));
-    container.lastElementChild.classList.add('grid-item');
-}
-
-const squares = document.querySelectorAll('.grid-item');
-squares.forEach((div) => {
-    div.addEventListener('mouseover', function(e) {
-        e.target.classList.add('active');
-    });
+const newGridBtn = document.querySelector('.top-container button');
+newGridBtn.addEventListener('click', function() {
+    createNewGrid(userInput());
 });
 
+createNewGrid(16);
 
+function addSqrEventListener() {
+    const squares = document.querySelectorAll('.grid-item');
+    squares.forEach((div) => {
+        div.addEventListener('mouseover', function(e) {
+            e.target.classList.add('active');
+        });
+    });
+}
+
+function createNewGrid(numberOfSquares) {
+
+    if (numberOfSquares > 100) {
+        alert("The amount of squares per side can't be higher than 100!");
+        return;
+
+    } else {
+        container.textContent = '';
+        container.setAttribute('style', `grid-template-columns: repeat(${numberOfSquares}, auto)`);
+        for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+            container.appendChild(document.createElement('div'));
+            container.lastElementChild.classList.add('grid-item');
+        }
+    }
+    addSqrEventListener();
+}
+
+function userInput() {
+    return Number(prompt('Enter the number of squares per side: '));
+}
